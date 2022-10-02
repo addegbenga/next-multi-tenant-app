dc31a2ce52183c8a4fcbb3c39169b0f1ebcf6c16
Adeyemi <addegbenga@gmail.com>
2022-10-02 22:47:30 +0100

    added config

--- middleware.ts
+++ middleware.ts
@@ -34,12 +34,10 @@ export default function middleware(req: NextRequest) {
       still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname
-          .replace(`.vercel.app`, "")
-          .replace(`.platformize.vercel.app`, "")
+      ? hostname.replace(`.xyz`, "").replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
   // rewrites for app pages
-  if (currentHost == "next-multi-tenant-app") {
+  if (currentHost == "startblogging") {
     if (
       url.pathname === "/login" &&
       (req.cookies.get("next-auth.session-token") ||
